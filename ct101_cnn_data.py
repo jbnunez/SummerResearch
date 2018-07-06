4210f1628892e8894b82d9f77d0786fb6087f9d0
Joe Nunez <joenunez@WL-215-191.WPA.Claremont.Edu>
2018-07-05 18:08:53 -0700

    ad

--- ct101_cnn_data.py
+++ ct101_cnn_data.py
@@ -32,10 +32,12 @@ for i in range(c):
 					images.append(im_arr)
 					labels.append(C)
 			C += 1
-labels = np.array(labels)
+labs = np.array(labels)
+del labels
+del categories
+
 print("==> Read in "+str(C)+" image categories")
 print("==> Padding images")
-
 
 #pad images
 shapes1 = [image.shape[0] for image in images]
@@ -44,6 +46,8 @@ dim1 = np.max(shapes1)
 dim2 = np.max(shapes2)
 N = len(images)
 padded = np.zeros((N, dim1, dim2, 3))
+del shapes1
+del shapes2
 
 for i in range(N):
 	idim1, idim2 = images[i].shape[0:2]
@@ -57,7 +61,7 @@ for i in range(N):
 		padded[i, start1:start1+idim1, start2:start2+idim2, 0] = images[i]
 	if i%1000 == 0:
 		print("==> Padded "+str(i)+" images")
-images = None
+del images 
 print('==> Making one-hot target vectors')
 #make targets into one-hot vectors
 # targets = np.zeros((N, C))
@@ -68,13 +72,22 @@ print('==> Making one-hot target vectors')
 # 	if i%1000 == 0:
 # 		print("==> Made "+str(i)+" target vectors")
 #this is the coolest bit of python golf I've seen in a while
-targets = np.eye(C)[labels]
+eye = np.eye(C)
+targets = eye[labs]
+del labs
+eye=None
 
 #images = np.array(padded)
-print("==> Making test-train split")
+# print("==> Making test-train split")
+# test_ind = np.random.choice(N//5)
+# X_test = padded[test_ind]
+# y_test = targets[test_ind]
+# X_train = np.delete(padded, test_ind)
+# y_train = np.delete(targets, test_ind)
+
 X_train, X_test, y_train, y_test = model_selection.train_test_split(padded,
 	targets, test_size=0.2)
-targets = None
-padded = None
+del targets
+del padded
 
 print('==> Loaded '+str(N)+' images')
