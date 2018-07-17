38dc44740c04f76434ede90c831266075312e3e7
Joe Nunez <joenunez@WL-215-191.WPA.Claremont.Edu>
2018-07-17 15:06:56 -0700

    auto

--- autoencoded_cancer_mlp.py
+++ autoencoded_cancer_mlp.py
@@ -3,24 +3,27 @@ import keras
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation
 from keras.layers import Input
-import cancer_encoded_generator_class as DataGenerator
+from cancer_encoded_generator_class import DataGenerator
 import pickle
+from  sklearn.model_selection import train_test_split
+import h5py
+
 
 
 
 num_epochs = 10
 steps_per_epoch = 1000
 
-
+input_shape = (500,)
 
 IDs = pickle.load(open("cancer_IDs", "rb"))
 train_IDs, test_IDs = train_test_split(IDs, test_size=0.2)
 train_IDs, val_IDs = train_test_split(train_IDs, test_size=0.15)
 
 
-params = {'dim': (600), 'batch_size': 30 , 'shuffle': True, 
-    conv_encoder_path='cancer_encoder.h5', 
-    covmat_encoder_path='cancer_cov_encoder.h5'}
+params = {'dim': input_shape, 'batch_size': 30 , 'shuffle': True, 
+    'conv_encoder_path': 'cancer_encoder_bce.h5', 
+    'covmat_encoder_path': 'cancer_cov_encoder.h5'}
 
 label_key = 'patient_tss'
 # label_key = 'patient_ts'
@@ -41,7 +44,7 @@ num_classes = train_generator.get_num_classes()
 print('==>Building Model.')
 
 model = Sequential()
-model.add(Dense(800, activation='relu'))
+model.add(Dense(800, activation='relu', input_shape=input_shape))
 model.add(Dense(800, activation='relu'))
 model.add(Dropout(0.2))
 model.add(Dense(800, activation='relu'))
@@ -54,7 +57,7 @@ model.compile(loss='mean_squared_error', optimizer='adam')
 
 
 print('==>Fitting Model.')
-model.fit_generator(generator=training_generator, 
+model.fit_generator(generator=train_generator, 
     steps_per_epoch=steps_per_epoch, epochs=num_epochs, 
     validation_data=val_generator, validation_steps=100,
     use_multiprocessing=True, workers=4)
@@ -64,5 +67,5 @@ print(model.evaluate_generator(generator=test_generator,
     workers=4, use_multiprocessing=True, verbose=1))
 
 
-
+model.save("autoencoded_cancer_mlp.h5")
 
--- cancer_encoded_generator_class.py
+++ cancer_encoded_generator_class.py
@@ -24,16 +24,18 @@ class DataGenerator(keras.utils.Sequence):
         self.dim = dim
         self.batch_size = batch_size
         self.labels = cancer_labels.label_options[label_key]
-        self.target_dim = self.labels[self.labels.index[0]].shape
+        self.imdim = (128,128)
+        self.target_dim = self.labels[self.labels.index[0]].shape[0]
         self.list_IDs = self.filter_IDs(list_IDs)
-        self.n_channels = n_channels
         self.conv_encoder = load_model(conv_encoder_path)
+        self.conv_encoder.summary()
         self.covmat_encoder = load_model(covmat_encoder_path)
+        self.covmat_encoder.summary()
         self.shuffle = shuffle
         self.on_epoch_end()
 
     def get_num_classes(self):
-        return self.target_dim[0]
+        return self.target_dim
 
     def filter_IDs(self, list_IDs):
         keep = self.labels.index
@@ -53,7 +55,7 @@ class DataGenerator(keras.utils.Sequence):
 
 
     def rgbToGray(self, rgbMat):
-        grayMat = np.empty((*self.dim, self.n_channels), 
+        grayMat = np.empty(*self.dim, 
             dtype=np.float32)
         r,g,b = rgbMat[:,:,0], rgbMat[:,:,1], rgbMat[:,:,2]
         r,g,b = r.astype(np.float32), g.astype(np.float32), b.astype(np.float32)
@@ -63,12 +65,15 @@ class DataGenerator(keras.utils.Sequence):
 
     def __data_generation(self, list_IDs_temp):
         'Generates data containing batch_size samples' 
-        # X : (n_samples, *dim, n_channels)
+        # X : (n_samples, *dim)
         # Initialization
+        conv_input = np.empty((self.batch_size, 128,128,1), dtype=np.float32)
+        covmat_input = np.empty((self.batch_size, 273), dtype=np.float32)
+
         X = np.empty((self.batch_size, *self.dim), 
-            dtype=np.float32)#np.uint8)
+            dtype=np.float32)
         y = np.empty((self.batch_size, self.target_dim), 
-            dtype=np.float32)#np.uint8)
+            dtype=np.float32)
 
         path = '../desktop/cancer/TCGA-GBM/'
 
@@ -80,24 +85,25 @@ class DataGenerator(keras.utils.Sequence):
 
             ds = pydicom.dcmread(path+ID)
             temp = ds.pixel_array
-            resized = resize(temp, self.dim, mode='constant')#, preserve_range=True)
+            resized = resize(temp, self.imdim, mode='constant')#, preserve_range=True)
             if resized.shape == (128,128,3):
                 resized = self.rgbToGray(resized)
             if resized.shape != (128, 128, 1) and resized.shape != (128, 128):
                 print("unrecognized shape", resized.shape)
             resized = resized.reshape(128,128).astype(np.float32)#.astype(np.uint8)
-            channeled = resized.reshape(128,128,1)
-            conv_encoded = self.conv_encoder.predict(channeled)
+            conv_input[i,:,:,:] = resized.reshape(1,128,128,1)
 
             im_cov = itc.im_to_cov(resized) #dim=16x16 (4x4 kernel)
             vec, mat = SE3.CovToMatVec(im_cov) #dim
             evecs = mat.flatten()
             norm = SE3.norm(mat,vec)
-            concat = np.hstack((evecs, vec, norm))
-            covmat_encoded = self.covmat_encoder.predict(concat)
-            
-            X[i,:,:,:] = np.hstack((conv_encoded, covmat_encoded))
+            covmat_input[i,:] = np.hstack((evecs, vec, norm))
+
             y[i] = self.labels[shortened_IDs[i]]
+
+        #conv_encoded = self.conv_encoder.predict(X=conv_input)
+        covmat_encoded = self.covmat_encoder.predict(covmat_input)
+        X[i,:] = np.hstack((conv_encoded, covmat_encoded))
 
 
         return X, y
